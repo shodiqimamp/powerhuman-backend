c7797b10a5891e3ab4bf42f5c76bbad0cd5a9605
shodiqimamp <shodiqimamp@gmail.com>
2023-02-24 12:31:21 +0700

    add relation to models and add response formatter

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use PhpParser\Node\Stmt\Return_;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'logo',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function team()
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    public function role()
+    {
+        return $this->hasMany(Role::class);
+    }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'company_id',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+    public function responsibilties()
+    {
+        return $this->hasMany(Responsibility::class);
+    }
 }
