1a4d6c67abd111d8c24cb2f3d3c48992cacdd2b3
shodiqimamp <shodiqimamp@gmail.com>
2023-02-26 11:26:24 +0700

    add seeder factories and create first API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Employee;
+use App\Models\Responsibility;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,10 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Company::factory(10)->create();
+        // // Team::factory(5)->create();
+        // // Role::factory(50)->create();
+        // Responsibility::factory(200)->create();
+        Employee::factory(1000)->create();
     }
 }
---
+++ database/seeders/UserCompanySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserCompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 1; $i <= 30; $i++) {
+            DB::table('company_user')->insert([
+                'user_id' => rand(1, 10),
+                'company_id' => rand(1, 10),
+            ]);
+        }
+    }
+}
