9447b3b0beea651b0ba3bb1ae7ab53e12b7cf9fa
shodiqimamp <shodiqimamp@gmail.com>
2023-02-28 10:54:29 +0700

    add company controller and API

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -6,26 +6,35 @@ use App\Models\Company;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateCompanyRequest;
+use App\Http\Requests\UpdateCompanyRequest;
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Auth;
 
 class CompanyController extends Controller
 {
-    public function all(Request $request)
+    public function fetch(Request $request)
     {
         $id = $request->input('id');
         $name = $request->input('name');
         $limit = $request->input('limit', 10);
 
+        $companyQuery = Company::with(['users'])->whereHas('users', function ($query) {
+            $query->where('user_id', Auth::id());
+        });
+
         if ($id) {
-            $company =  Company::with(['users'])->find($id);
+            $company = $companyQuery->find($id);
 
             if ($company) {
                 return ResponseFormatter::success($company, 'Company Found');
             }
 
-            return ResponseFormatter::error('Compnay Not Found', 404);
+            return ResponseFormatter::error('Company Not Found', 404);
         }
 
-        $companies = Company::with(['users']);
+        $companies = $companyQuery;
 
         if ($name) {
             $companies->where('name', 'like', '%' . $name . '%');
@@ -35,5 +44,60 @@ class CompanyController extends Controller
             $companies->paginate($limit),
             'Companies Found'
         );
+    }
+
+
+    public function create(CreateCompanyRequest $request)
+    {
+        try {
+            if ($request->hasFile('logo')) {
+                $path = $request->file('logo')->store('public/logos');
+            }
+
+            $company = Company::create([
+                'name' => $request->name,
+                'logo' => $path,
+            ]);
+
+            if (!$company) {
+                throw new Exception('Company Not Created');
+            }
+
+            $user = User::find(Auth::id());
+            $user->company()->attach($company->id);
+
+            $company->load('users');
+
+            return ResponseFormatter::success($company, 'Company Created');
+        } catch (Exception $error) {
+
+            return ResponseFormatter::error($error->getMessage(), 500);
+        }
+    }
+
+
+    public function update(UpdateCompanyRequest $request, $id)
+    {
+
+        try {
+            $company = Company::find($id);
+
+            if (!$company) {
+                throw new Exception('Company Not Found');
+            }
+
+            if ($request->hasFile('logo')) {
+                $path = $request->file('logo')->store('public/logos');
+            }
+
+            $company->update([
+                'name' => $request->name,
+                'logo' => $path,
+            ]);
+
+            return ResponseFormatter::success($company, 'Company Updated');
+        } catch (Exception $error) {
+            return ResponseFormatter::error($error->getPrevious(), 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware('auth:sanctum')->prefix('company')->name('company.')->group(function () {
+    Route::get('', [CompanyController::class, 'fetch'])->name('fetch');
+    Route::post('', [CompanyController::class, 'create'])->name('create');
+    Route::post('/update/{id}', [CompanyController::class, 'update'])->name('update');
 });
 
 
-Route::get('/company', [CompanyController::class, 'all']);
+Route::name('auth.')->group(function () {
+    Route::post('login', [UserController::class, 'login'])->name('login');
+    Route::post('register', [UserController::class, 'register'])->name('register');
 
-Route::post('login', [UserController::class, 'login']);
-Route::post('register', [UserController::class, 'register']);
-Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('user', [UserController::class, 'fetch'])->middleware('auth:sanctum');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [UserController::class, 'logout'])->name('logout');
+        Route::get('user', [UserController::class, 'fetch'])->name('fetch');
+    });
+});
